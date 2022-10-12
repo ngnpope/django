6dbb561326c31077b8492dd2f62988772447d014
Nick Pope <nick@nickpope.me.uk>
2022-10-12 20:48:58 +0100

    Simplified `django.utils.dateformat.DateFormat.t()`.
    The value can only be one of 28, 29, 30, or 31.
    
    As such, there is no need to zero-pad to a width of two.

--- django/utils/dateformat.py
+++ django/utils/dateformat.py
@@ -291,7 +291,7 @@ class DateFormat(TimeFormat):
 
     def t(self):
         "Number of days in the given month; i.e. '28' to '31'"
-        return "%02d" % calendar.monthrange(self.data.year, self.data.month)[1]
+        return calendar.monthrange(self.data.year, self.data.month)[1]
 
     def U(self):
         "Seconds since the Unix epoch (January 1 1970 00:00:00 GMT)"
